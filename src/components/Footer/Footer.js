18b4bdf15c86702d25e2525d65ad914dc7eb4376
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-22 16:27:15 -0600

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import './footer.scss';
+import React from "react";
+import "./footer.scss";
+import {
+  FaFacebookSquare,
+  FaTwitter,
+  FaInstagram,
+  FaDiscord,
+  FaPinterest,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer__socials">
+        <span>
+          <FaTwitter />
+        </span>
+        <span>
+          <FaFacebookSquare />
+        </span>
+        <span>
+          <FaInstagram />
+        </span>
+        <span>
+          <FaDiscord />
+        </span>
+        <span>
+          <FaPinterest />
+        </span>
+      </div>
       &#169;2022 by Blazed Kid
     </div>
   );
