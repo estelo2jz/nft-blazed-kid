8cb3181c30484279fb1b0ad304ca110a859752c9
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-25 16:11:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Header from "./components/Header";
-import Products from "./components/Products";
+// import Header from "./components/Header";
+import Products from "./components/NFTs";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { DataNFTProvider } from "./components/DataNFTProvider";
+import { DataProvider } from "./components/Shop/DataProvider";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 
+import ShopDetails from './components/Shop/ShopDetails'
+
 import Nav from "./components/Navigation/Nav";
-import SubHeader from './components/Navigation/SubHeader'
+import SubHeader from "./components/Navigation/SubHeader";
 import Main from "./components/MainPage/Main";
 import Home from "./components/Home/Home";
 import NFT from "./components/NFT/NFT";
@@ -18,29 +21,32 @@ import Shop from "./components/Shop/Shop";
 
 function App() {
   return (
-    <DataNFTProvider>
-      <div className="App">
-        <ScrollToTop />
-        <Router>
-          <Nav />
-          <SubHeader />
-          <section>
-            <Routes>
-              <Route path="nft" element={<Products />} />
-              <Route path="nft/:id" element={<Details />} />
-              <Route path="cart" element={<Cart />} />
+    <DataProvider>
+      <DataNFTProvider>
+        <div className="App">
+          <ScrollToTop />
+          <Router>
+            <Nav />
+            <SubHeader />
+            <section>
+              <Routes>
+                <Route exact path="/nft" element={<Products />} />
+                <Route exact path="/nft/:id" element={<Details />} />
+                <Route exact path="/shop" element={<Shop />} />
+                <Route exact path="/shop/:id" element={<ShopDetails />} />
+                <Route exact path="/cart" element={<Cart />} />
 
-              <Route exact path="/" element={<Home />} />
-              {/* <Route exact path="/home" element={<Home />} /> */}
-              <Route exact path="/nft" element={<NFT />} />
-              <Route exact path="/shop" element={<Shop />} />
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
-          </section>
-        </Router>
-        <Footer />
-      </div>
-    </DataNFTProvider>
+                <Route exact path="/" element={<Home />} />
+                {/* <Route exact path="/home" element={<Home />} /> */}
+                <Route exact path="/nft" element={<NFT />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </section>
+          </Router>
+          <Footer />
+        </div>
+      </DataNFTProvider>
+    </DataProvider>
   );
 }
 
