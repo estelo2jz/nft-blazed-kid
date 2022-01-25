f90a34e4ac5b1b8d15ef6b0319084893da53db28
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-25 16:11:23 -0600

    Update Shop.js

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import { DataContext } from './DataProvider'
+import { Link } from 'react-router-dom'
 
-const Shop = () => {
+export default function Shop() {
+  const value = useContext(DataContext)
+  const [products] = value.products
+  const addCart = value.addCart
+
   return (
-    <div>SHOP</div>
-  );
-};
+    <div className="products">
+      {
+        products.map(product => (
+          <div className="card" key={product._id}>
+            <Link to={`/shop/${product._id}`}>
+              <img src={product.images[0]} alt="" />
+            </Link>
+            <div className="box">
+              <h3 title={product.title}>
+                <Link to={`/shop/${product._id}`}>{product.title}</Link>
+              </h3>
+              <p>{product.description}</p>
+              <h4>${product.price}</h4>
+              <button onClick={() => addCart(product._id)}>
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))
+      }
 
-export default Shop;
+
+    </div>
+  )
+}
