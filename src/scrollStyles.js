f3dcdd83583bc4159e5668055edaee2236780a21
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-25 08:58:39 -0600

    Update scrollStyles.js

--- src/scrollStyles.js
+++ src/scrollStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Button = styled.div`
    position: fixed; 
    width: 100%;
-   left:48.3%;
+   left: 10%;
    bottom: 40px;
    height: 100px;
    font-size: 3rem;
