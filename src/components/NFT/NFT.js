14a3ff3cda658ad79dfa588bfa1488a23b9de316
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-25 14:31:04 -0600

    Update NFT.js

--- src/components/NFT/NFT.js
+++ src/components/NFT/NFT.js
@@ -8,20 +8,6 @@ const NFT = () => {
   return (
       <div className="about">
         <Products />
-        {/* {NFTData.map((item, index) => {
-        return (
-          <Link to={item.path}>
-            <div key={index} className="about__nft-container">
-              <div className="about__nft-heading">
-                <h3>{item.title}</h3>
-              </div>
-              <div className="about__nft-img">
-                <img src={item.img} alt="NTF's" />
-              </div>
-            </div>
-          </Link>
-        );
-      })} */}
       </div>
   );
 };
