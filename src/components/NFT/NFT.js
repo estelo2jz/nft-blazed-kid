c04425df1193fba0908a43719f169c9d99ac144e
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-25 16:11:12 -0600

    Update NFT.js

--- src/components/NFT/NFT.js
+++ src/components/NFT/NFT.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import NFTData from "./Data/data";
-import Products from "../Products";
+import Products from "../NFTs";
 import "./nft.scss";
 
 const NFT = () => {
   return (
       <div className="about">
-        <Products />
+        {/* <Products /> */}
       </div>
   );
 };
