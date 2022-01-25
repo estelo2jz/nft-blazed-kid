adfe02dec0a41918489c3de64a2977d37e22e641
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-25 16:11:24 -0600

    Update Products.js

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -12,12 +12,12 @@ export default function Products() {
       {
         products.map(product => (
           <div className="card" key={product._id}>
-            <Link to={`/products/${product._id}`}>
+            <Link to={`/shop/${product._id}`}>
               <img src={product.images[0]} alt="" />
             </Link>
             <div className="box">
               <h3 title={product.title}>
-                <Link to={`/products/${product._id}`}>{product.title}</Link>
+                <Link to={`/shop/${product._id}`}>{product.title}</Link>
               </h3>
               <p>{product.description}</p>
               <h4>${product.price}</h4>
