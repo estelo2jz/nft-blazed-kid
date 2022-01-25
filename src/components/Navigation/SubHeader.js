d723717aac3aed5429060aae39734a28b7e2d104
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-25 16:11:07 -0600

    Update SubHeader.js

--- src/components/Navigation/SubHeader.js
+++ src/components/Navigation/SubHeader.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import Cart from "../svg/cart.svg";
 import { Link } from "react-router-dom";
+import { DataContext } from "../Shop/DataProvider";
 
 import "./subHeader.scss";
 
 const SubHeader = () => {
+  const [menu, setMenu] = useState(false);
+  const value = useContext(DataContext);
+  const [cart] = value.cart;
   return (
     <div className="subHeader">
       <div className="subHeader__nav">
@@ -16,6 +21,12 @@ const SubHeader = () => {
         <span>
           <Link to="/shop">SHOP</Link>
         </span>
+        <div className="cart-icon">
+          <span>{cart.length}</span>
+          <Link to="/cart">
+            <img src={Cart} alt="" width="30" />
+          </Link>
+        </div>
       </div>
     </div>
   );
