ebfa81711e3052d40749e32e672c8c5eedbc3fd9
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-26 12:05:01 -0600

    Update ShopDetails.js

--- src/components/Shop/ShopDetails.js
+++ src/components/Shop/ShopDetails.js
@@ -1,60 +1,80 @@
-import React, { useContext, useState, useRef } from 'react'
-import { useParams } from 'react-router-dom'
-import { DataContext } from './DataProvider'
-import Sizes from './Sizes'
-import DetailsThumb from './ShopDetailsThumb'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { DataContext } from "./DataProvider";
+import Sizes from "./Sizes";
+import DetailsThumb from "./ShopDetailsThumb";
+import { Link } from "react-router-dom";
+import "./shopDetails.scss";
 
 export default function Details() {
   const { id } = useParams();
-  const value = useContext(DataContext)
-  const [products] = value.products
-  const addCart = value.addCart
+  const value = useContext(DataContext);
+  const [products] = value.products;
+  const addCart = value.addCart;
 
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
   const imgDiv = useRef();
 
-
   const details = products.filter((product, index) => {
-    return product._id === id
-  })
+    return product._id === id;
+  });
 
-  const handleMouseMove = e => {
+  const handleMouseMove = (e) => {
     const { left, top, width, height } = e.target.getBoundingClientRect();
-    const x = (e.pageX - left) / width * 100
-    const y = (e.pageY - top) / height * 100
-    imgDiv.current.style.backgroundPosition = `${x}% ${y}%`
-  }
-
+    const x = ((e.pageX - left) / width) * 100;
+    const y = ((e.pageY - top) / height) * 100;
+    imgDiv.current.style.backgroundPosition = `${x}% ${y}%`;
+  };
 
   return (
     <>
-      {
-        details.map(product => {
-          return (
-            <div className="details" key={product._id}>
-              <div className="img-container" onMouseMove={handleMouseMove}
-                 ref={imgDiv}
-                onMouseLeave={() => imgDiv.current.style.backgroundPosition = `center`}>
-                  <img src={product.images[index]} alt="" />
-                </div>
+      {details.map((product) => {
+        return (
+          <div className="shop__details" key={product._id}>
+            <div
+              className="shop__img-container"
+              onMouseMove={handleMouseMove}
+              ref={imgDiv}
+              onMouseLeave={() =>
+                (imgDiv.current.style.backgroundPosition = `center`)
+              }
+            >
+              <img src={product.images[index]} alt="" />
+            </div>
 
-              <div className="box-details">
+            <div className="shop__box-details">
+              <div className="shop__box-details__title">
                 <h2 title={product.title}>{product.title}</h2>
-                <h3>${product.price}</h3>
-                <Sizes sizes={product.sizes} />
+              </div>
+              <div className="shop__box-details__desc">
                 <p>{product.description}</p>
                 <p>{product.content}</p>
+              </div>
+              <div className="shop__box-details__imgs">
                 <DetailsThumb images={product.images} setIndex={setIndex} />
-                <Link to="/cart" className="cart" onClick={() => addCart(product._id)}>
-                  Add to cart
+              </div>
+              <div className="shop__box-details__sizes">
+                <Sizes sizes={product.sizes} />
+              </div>
+              <div className="shop__box-details__price">
+                <h3>${product.price}</h3>
+              </div>
+              <div className="shop__box-details__cart">
+                  {/* to="/cart" */}
+                <Link
+                  to=""
+                  className="shop__cart"
+                  onClick={() => addCart(product._id)}
+                >
+                  <button>
+                    Add to cart
+                  </button>
                 </Link>
               </div>
-
             </div>
-          )
-        })
-      }
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
