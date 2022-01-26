7d6d2f3bd9fea547b1b9e3f70d1dc13b4e1d0a1e
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-26 12:05:10 -0600

    Update Shop.js

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,35 +1,34 @@
-import React, { useContext } from 'react'
-import { DataContext } from './DataProvider'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { DataContext } from "./DataProvider";
+import { Link } from "react-router-dom";
+import "./shop.scss";
 
 export default function Shop() {
-  const value = useContext(DataContext)
-  const [products] = value.products
-  const addCart = value.addCart
+  const value = useContext(DataContext);
+  const [products] = value.products;
+  const addCart = value.addCart;
 
   return (
-    <div className="products">
-      {
-        products.map(product => (
-          <div className="card" key={product._id}>
+    <div className="shop">
+      {products.map((product) => (
+        <div className="shop__card" key={product._id}>
+          <div className="shop__card__img">
             <Link to={`/shop/${product._id}`}>
               <img src={product.images[0]} alt="" />
             </Link>
-            <div className="box">
-              <h3 title={product.title}>
-                <Link to={`/shop/${product._id}`}>{product.title}</Link>
-              </h3>
-              <p>{product.description}</p>
-              <h4>${product.price}</h4>
-              {/* <button onClick={() => addCart(product._id)}>
+          </div>
+          <div className="shop__box">
+            <h3 title={product.title}>
+              <Link to={`/shop/${product._id}`}>{product.title}</Link>
+            </h3>
+            <p>{product.description}</p>
+            <h4>${product.price}</h4>
+            {/* <button onClick={() => addCart(product._id)}>
                 Add to cart
               </button> */}
-            </div>
           </div>
-        ))
-      }
-
-
+        </div>
+      ))}
     </div>
-  )
+  );
 }
