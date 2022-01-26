9d65a5b4316a2459076bbbada00d44695f22b087
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-26 12:04:46 -0600

    Update DetailsThumb.js

--- src/components/DetailsThumb.js
+++ src/components/DetailsThumb.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function DetailsThumb({ images, setIndex }) {
   return (
-    <div className="thumb">
+    <div className="nft__details__thumb">
       {
         images.map((img, index) => (
           <img src={img} alt="" key={index}
