56ac63022b4f02f26131cce30d53f668dec1ca6f
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-28 11:35:48 -0600

    Update ShopDetails.js

--- src/components/Shop/ShopDetails.js
+++ src/components/Shop/ShopDetails.js
@@ -6,6 +6,8 @@ import DetailsThumb from "./ShopDetailsThumb";
 import { Link } from "react-router-dom";
 import "./shopDetails.scss";
 
+import Featured from "../Shop/Items/Featured/Featured";
+
 export default function Details() {
   const { id } = useParams();
   const value = useContext(DataContext);
@@ -27,54 +29,57 @@ export default function Details() {
   };
 
   return (
-    <>
-      {details.map((product) => {
-        return (
-          <div className="shop__details" key={product._id}>
-            <div
-              className="shop__img-container"
-              onMouseMove={handleMouseMove}
-              ref={imgDiv}
-              onMouseLeave={() =>
-                (imgDiv.current.style.backgroundPosition = `center`)
-              }
-            >
-              <img src={product.images[index]} alt="" />
-            </div>
+    <div>
+      <>
+        {details.map((product) => {
+          return (
+            <div className="shop__details" key={product._id}>
+              <div
+                className="shop__img-container"
+                onMouseMove={handleMouseMove}
+                ref={imgDiv}
+                onMouseLeave={() =>
+                  (imgDiv.current.style.backgroundPosition = `center`)
+                }
+              >
+                <img src={product.images[index]} alt="" />
+              </div>
 
-            <div className="shop__box-details">
-              <div className="shop__box-details__title">
-                <h2 title={product.title}>{product.title}</h2>
-              </div>
-              <div className="shop__box-details__desc">
-                <p>{product.description}</p>
-                <p>{product.content}</p>
-              </div>
-              <div className="shop__box-details__imgs">
-                <DetailsThumb images={product.images} setIndex={setIndex} />
-              </div>
-              <div className="shop__box-details__sizes">
-                <Sizes sizes={product.sizes} />
-              </div>
-              <div className="shop__box-details__price">
-                <h3>${product.price}</h3>
-              </div>
-              <div className="shop__box-details__cart">
+              <div className="shop__box-details">
+                <div className="shop__box-details__title">
+                  <h2 title={product.title}>{product.title}</h2>
+                </div>
+                <div className="shop__box-details__desc">
+                  <p>{product.description}</p>
+                  <p>{product.content}</p>
+                </div>
+                <div className="shop__box-details__imgs">
+                  <DetailsThumb images={product.images} setIndex={setIndex} />
+                </div>
+                <div className="shop__box-details__sizes">
+                  <Sizes sizes={product.sizes} />
+                </div>
+                <div className="shop__box-details__price">
+                  <h3>${product.price}</h3>
+                </div>
+                <div className="shop__box-details__cart">
                   {/* to="/cart" */}
-                <Link
-                  to="/cart"
-                  className="shop__cart"
-                  onClick={() => addCart(product._id)}
-                >
-                  <button>
-                    Add to cart
-                  </button>
-                </Link>
+                  <Link
+                    to="/cart"
+                    className="shop__cart"
+                    onClick={() => addCart(product._id)}
+                  >
+                    <button>Add to cart</button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </>
+          );
+        })}
+      </>
+      <div>
+        <Featured />
+      </div>
+    </div>
   );
 }
