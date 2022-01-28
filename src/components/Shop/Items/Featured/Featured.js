271f49f9fa1bc4ef93807a4af05da131fa885d4c
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-01-28 11:32:47 -0600

    Create Featured.js

---
+++ src/components/Shop/Items/Featured/Featured.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { DataContext } from "../../DataProvider";
+import { Link } from "react-router-dom";
+import "./featured.scss";
+
+export default function Featured() {
+  const value = useContext(DataContext);
+  const [products] = value.products;
+  const addCart = value.addCart;
+
+  return (
+    <div className="featured">
+      {products.slice(9, 15).map((product) => (
+        <div className="featured__card" key={product._id}>
+          <div className="featured__card__img">
+          <Link to={`/shop/${product._id}`}>
+            <img src={product.images[0]} alt="" />
+          </Link>
+          </div>
+          <div className="featured__box">
+            <h4 title={product.title}>
+              <Link to={`/shop/${product._id}`}>{product.title}</Link>
+            </h4>
+            <p>{product.description}</p>
+            <h4>${product.price}</h4>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
