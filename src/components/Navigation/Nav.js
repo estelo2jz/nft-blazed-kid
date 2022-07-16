434966d4ac0590860d6a0d90092e8f6391535608
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-07-16 18:05:43 -0500

    Update Nav.js

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -5,13 +5,14 @@ import IMG from "./logo.jpeg";
 import IMG2 from "./Logo2.jpeg";
 import IMG3 from "./logo6.gif";
 import IMG4 from "./bkid.gif";
+import BK from './BK3.png';
 
 const Nav = () => {
   return (
     <div className="nav__container">
       <header>
         <Link to="/" class="logo">
-          <img src={IMG4} alt="logo" />
+          <img src={BK} alt="logo" />
         </Link>
         {/* <a href="#home" class="logo">
           <img src={IMG4} alt="logo" />
