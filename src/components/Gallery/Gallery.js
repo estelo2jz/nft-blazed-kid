0d95239e2c760d4d33fd3417a8c88a2be1452cce
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2022-08-27 21:32:19 -0500

    Create Gallery.js

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NFTDataOne from "../Home/data";
+import "./gallery.scss";
+
+function Gallery() {
+  return (
+    <div className="gallery__container">
+      <div id="overview" className="gallery__section-content">
+        {NFTDataOne.map((item, index) => {
+          return (
+            <Link to="/nft">
+              <div key={index} className="gallery__nft-container">
+                <div className="gallery__nft-heading">
+                  <h3>{item.title}</h3>
+                </div>
+                <div className="gallery__nft-img">
+                  <img src={item.img} alt="NTF's" />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Gallery;
