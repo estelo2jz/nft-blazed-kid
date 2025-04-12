2aa1025f582ed46db725ef6a9d99339f5f49da51
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2025-04-12 13:47:11 -0500

    Update vite.condig.ts

--- vite.condig.ts
+++ vite.condig.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  
 })
+
+
 
 function defineConfig(arg0: { plugins: Plugin<any>[][] }) {
   throw new Error('Function not implemented.')
